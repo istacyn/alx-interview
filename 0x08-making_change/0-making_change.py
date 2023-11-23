b80eec83a10ca75badd2414895a43e9af7b55d38
Ivy <ivystacyn@gmail.com>
2023-11-23 12:30:09 +0000

    Making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -21,15 +21,11 @@ def makeChange(coins, total):
     if total == 0:
         return 0
 
-    coins.sort()
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
 
-    arr = [float('inf')] * (total + 1)
-    arr[0] = 0
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin > i:
-                break
-            arr[i] = min(arr[i], arr[i - coin] + 1)
-
-    return arr[total] if arr[total] != float('inf') else -1
+    return dp[total] if dp[total] != float('inf') else -1
