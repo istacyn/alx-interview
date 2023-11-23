c12e0fef996e5794050d43043d54c128cfe5461c
Ivy <ivystacyn@gmail.com>
2023-11-23 13:48:20 +0000

    Making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -21,17 +21,11 @@ def makeChange(coins, total):
     if total == 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    '''for coin in coins:
-        for value in range(coin, total + 1):
-            dp[value] = min(dp[value], dp[value - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1 '''
+    arr = [float('inf')] * (total + 1)
+    arr[0] = 0
 
     for coin in coins:
         for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+            arr[i] = min(arr[i], arr[i - coin] + 1)
 
-    return dp[total] if dp[total] != float('inf') else -1
+    return arr[total] if arr[total] != float('inf') else -1
