5c16587c98dc2d92b23de58c8fb26814f7736434
Ivy <ivystacyn@gmail.com>
2023-11-24 06:21:33 +0000

    Making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -18,17 +18,6 @@ def makeChange(coins, total):
     - int: Fewest number of coins needed to meet the total amount.
            or -1 if total cannot be met by any combination of available coins.
     """
-    """if total <= 0:
-        return 0
-
-    arr = [float('inf')] * (total + 1)
-    arr[0] = 0
-
-    for coin in coins:
-        for i in range(coin, total + 1):
-            arr[i] = min(arr[i], arr[i - coin] + 1)
-
-    return arr[total] if arr[total] != float('inf') else -1"""
 
     if total <= 0:
         return 0
